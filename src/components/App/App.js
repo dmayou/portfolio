e8df76867695c305e5fe8adb00a78ae61b540c75
David Mayou <davidpmayou@gmail.com>
2018-12-15 13:50:00 -0600

    add mui components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import { createMuiTheme, Grid } from '@material-ui/core';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { withStyles } from '@material-ui/core/styles';
 import './App.css';
+import ButtonAppBar from '../ButtonAppBar/ButtonAppBar';
+import Project from '../Project/Project';
+
+import 'typeface-roboto';
+
+const styles = {
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+};
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <MuiThemeProvider theme={theme}>
+          <ButtonAppBar />
+          <Grid container>
+            <Project />        
+          </Grid>
+        </MuiThemeProvider>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
