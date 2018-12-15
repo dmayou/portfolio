1b15e25090ddb8df9f260eea5bb941a081975e87
David Mayou <davidpmayou@gmail.com>
2018-12-15 17:53:31 -0600

    add github and heroku icons

---
+++ src/components/GitHubIcon/GitHubIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GitHubIcon(props) { 
+    return (
+        <div>
+            <img src='/images/GitHub/GitHub-Mark-32px.png' alt='' />
+        </div>
+    );
+}
+
+export default GitHubIcon;
---
+++ src/components/HerokuLogo/HerokuLogo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HerokuLogo(props) {
+    return (
+        <div>
+            <img 
+                src='/images/Heroku/heroku-logotype-vertical-black.png' 
+                width="46px" height="46px" 
+                alt='' />
+        </div>
+    );
+}
+
+export default HerokuLogo;
