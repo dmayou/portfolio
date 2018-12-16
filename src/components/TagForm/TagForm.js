f691914efcbbbf6c9319718367e35e9f18ac0a33
David Mayou <davidpmayou@gmail.com>
2018-12-16 15:57:22 -0600

    add TagForm component

---
+++ src/components/TagForm/TagForm.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { Button, TextField } from '@material-ui/core';
+
+class TagForm extends Component {
+    state = {
+        newTag: ''
+    }
+    clearForm() {
+        console.log('in clearForm()');
+        this.setState({
+            newTag: ''
+        });
+    }
+    handleChange = (event) => {
+        this.setState({
+            newTag: event.target.value
+        });
+    }
+    handleClick = () => {
+        if (this.state.newTag) { // cannot be blank
+            this.props.dispatch({ type: 'ADD_TAG', payload: this.state.newTag });
+            this.clearForm();
+        } else {
+            this.displaySnackBar(
+                'You must enter a tag.',
+                3500
+            );
+        }
+    }
+    displaySnackBar = (msg, dwell) => {
+        this.props.dispatch({
+            type: 'SHOW_SNACKBAR',
+            payload: { message: msg, dwell: dwell }
+        });
+    }
+    render() {
+        return (
+            <form>
+                <TextField
+                    label='Name'
+                    value={this.state.newTag}
+                    onChange={this.handleChange}
+                />
+                <Button
+                    onClick={this.handleClick}
+                    variant="contained"
+                    default
+                >Add Tag
+                </Button>
+            </form>
+        );
+    }
+}
+
+export default connect()(TagForm);
